c912ac6ad82f411a63d0f0f2cee2a9a9e4697098
RatsBow <0730bss@gmail.com>
2023-08-31 18:24:53 +0900

    db.create_all()

--- main.py
+++ main.py
@@ -76,8 +76,8 @@ class Comment(db.Model):
     parent_post = relationship("BlogPost", back_populates="comments")
 
 
-# with app.app_context():
-#     db.create_all()
+with app.app_context():
+    db.create_all()
 
 
 # Creating Admin Only Decorator
